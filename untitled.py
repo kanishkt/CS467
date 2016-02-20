de61b73b1c64a5cb5c905c4ee17ac39b98f61f79
Kanishk Thareja <kanishkt@gmail.com>
2016-02-20 15:08:40 -0600

    Got all Post from Reddit Search

--- untitled.py
+++ untitled.py
@@ -9,7 +9,10 @@ def hello_world():
     r = praw.Reddit(user_agent='my_cool_application')
     submissions = r.get_subreddit('opensource').get_hot(limit=5)
     summary = [str(x) for x in submissions]
-    return summary
+    searchQ = r.search('sports', subreddit=None, sort=None, syntax=None, period=None)
+    for y in list(searchQ):
+        print y.title
+    return "hello"
 
 if __name__ == '__main__':
     app.run()
