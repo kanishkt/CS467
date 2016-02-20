46ff8800d43fd780db1d120eec2344b7eab9f001
Kanishk Thareja <kanishkt@gmail.com>
2016-02-20 14:34:05 -0600

    Initial commit

---
+++ untitled.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import praw
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    r = praw.Reddit(user_agent='my_cool_application')
+    submissions = r.get_subreddit('opensource').get_hot(limit=5)
+    summary = [str(x) for x in submissions]
+    return summary
+
+if __name__ == '__main__':
+    app.run()
