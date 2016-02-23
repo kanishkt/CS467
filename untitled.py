0efbef5c348cd62b6bd3a31c76ee410c386334c1
Kanishk Thareja <kanishkt@gmail.com>
2016-02-22 23:56:30 -0600

    Working 2

--- untitled.py
+++ untitled.py
@@ -16,7 +16,7 @@ soup = BeautifulSoup(page.text, "html.parser")
 @app.route('/')
 def hello_world():
     data = t.RedditData()
-    return flask.render_template("index.html" , data=data)
+    return flask.render_template("index.html", data=data)
 
 @app.route("/data")
 @app.route("/data/<int:ndata>")
