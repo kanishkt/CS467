b56f6f0b2166c1f0c289796f9267615735890dfc
Kanishk Thareja <kanishkt@gmail.com>
2016-02-23 19:50:46 -0600

    Competed V1.0

--- untitled.py
+++ untitled.py
@@ -1,10 +1,6 @@
 import flask
 from flask import Flask
 import scrape as t
-import json
-import numpy as np
-import requests as req
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -12,33 +8,37 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     data = t.RedditData("sports")
-    return flask.render_template("index.html", data=data)
+    return flask.render_template("index.html", data=data , title="Sports")
+
 
 @app.route("/tv")
 def getTV():
     data = t.RedditData("tv")
-    return flask.render_template("index.html", data=data)
+    return flask.render_template("index.html", data=data, title="TV")
+
 
 @app.route("/fashion")
 def getFashion():
     data = t.RedditData("fashion")
-    return flask.render_template("index.html", data=data)
+    return flask.render_template("index.html", data=data, title="Fashion" )
+
 
 @app.route("/movies")
 def getMovies():
     data = t.RedditData("movies")
-    return flask.render_template("index.html", data=data)
+    return flask.render_template("index.html", data=data, title="Movies")
+
 
 @app.route("/games")
 def getGames():
     data = t.RedditData("games")
-    return flask.render_template("index.html", data=data)
+    return flask.render_template("index.html", data=data, title="Games")
+
 
 @app.route("/politics")
 def getPolitics():
     data = t.RedditData("politics")
-    return flask.render_template("index.html", data=data)
-
+    return flask.render_template("index.html", data=data, title="Politics")
 
 
 if __name__ == '__main__':
